94881674faf54893655a8c139c89923ffc817df0
dilorc2 <dilorc2@gmail.com>
2016-08-10 14:20:56 -0500

    added new commponent search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
 const API_KEY = 'AIzaSyC-6rqknUnyht3rZVWvhdXbBYNsh98J9zw';
 
 
 const App = () => {
-  return <div>Hi</div>
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
