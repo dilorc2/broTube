ca707e40dffa3d5f48ed9b6a20466e4633a67a39
dilorc2 <dilorc2@gmail.com>
2016-08-10 17:15:22 -0500

    able to render video on page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-import SearchBar from './components/search_bar'
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyC-6rqknUnyht3rZVWvhdXbBYNsh98J9zw';
 
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+    YTSearch({key: API_KEY, term: 'broCoder'}, (videos) => {
+      this.setState({ videos });
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
