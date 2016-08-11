5b548257b699f4a86f12a127ed6af42ebb255328
dilorc2 <dilorc2@gmail.com>
2016-08-11 12:09:31 -0500

    brotube is finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -19,7 +20,7 @@ class App extends Component {
   }
 
   videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos) => {
+    YTSearch({key: API_KEY, term: "bro" + term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -28,6 +29,8 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300)
+
     return (
       <div>
         <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
